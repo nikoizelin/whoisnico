5ce1a4d1150e27d2f2e22b8fa6dcedcd35df84d7
nikoizelin <80548797+nikoizelin@users.noreply.github.com>
2025-05-22 17:57:53 +0200

    easteregg

--- src/app/easteregg.js
+++ src/app/easteregg.js
@@ -7,7 +7,7 @@ function EasterEgg({ isOpen, onClose }) {
       <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
         <div className="bg-white p-6 rounded-xl shadow-lg text-center max-w-sm text-black">
           <h2 className="text-xl font-bold mb-4">🎉 wow... you found the easter egg 🎉</h2>
-          <p className="mb-6">here's a cookie 🍪</p>
+          <p className="mb-6">here&apos;s a cookie 🍪</p>
           <button
             onClick={onClose}
             className="bg-gradient-to-r from-blue-500 via-red-500 to-green-500 hover:bg-gradient-to-l text-black font-semibold py-2 px-4 rounded"
